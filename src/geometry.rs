b80ec3cb7c2169544040b6a7e606ffca44362fbd
adamgj <adam@modsrus.com>
2020-09-09 00:48:45 -0400

    start switch to num genercis

--- src/geometry.rs
+++ src/geometry.rs
@@ -8,16 +8,23 @@ pub enum Axis {
 
 const ERR_2D_UNSUPP_AXIS: &str = "Unsupported Axis. 2D supports X and Y.";
 
-pub trait TCoordinate {
-    fn get_axis_value(&self, axis: Axis) -> &i32;
+pub trait TValue: PartialEq + PartialOrd + std::ops::MulAssign + Copy + Clone + num::Num + num::NumCast + num::One + num::Zero + num::Signed {}
+impl<T> TValue for T where T: PartialEq + PartialOrd + std::ops::MulAssign + Copy + Clone + num::Num + num::NumCast + num::One + num::Zero + num::Signed {}
 
-    fn set_axis_value(&mut self, axis: Axis, value: i32);
+
+pub trait TCoordinate<T>
+where T: TValue {
+    fn get_axis_value(&self, axis: Axis) -> &T;
+
+    fn set_axis_value(&mut self, axis: Axis, value: T);
 }
 
-pub trait TTransformable {
-    fn shift<T: TCoordinate>(&mut self, coord: &T);
+pub trait TTransformable<T>
+where T: TValue {
+    fn shift<U>(&mut self, coord: &U)
+    where U: TCoordinate<T>;
 
-    fn shift_axis(&mut self, axis: Axis, value: i32);
+    fn shift_axis(&mut self, axis: Axis, value: T);
 
     fn flip_axis(&mut self, axis1: Axis, axis2: Axis);
 
@@ -26,35 +33,55 @@ pub trait TTransformable {
     fn reflect_axis(&mut self, axis: Axis);
 }
 
-pub trait TPoint: PartialEq +  PartialOrd + Copy + Clone + Default + TTransformable + TCoordinate {}
-impl<T: PartialEq +  PartialOrd + Copy + Clone + Default + TTransformable + TCoordinate> TPoint for T {}
+// TODO impl Clone, Default, Eq, AddAssign etc.
+pub trait TPoint<T>: PartialEq + PartialOrd + Copy + Clone + Default + TTransformable<T> + TCoordinate<T> where T: TValue {}
+impl<U, T> TPoint<T> for U where
+    U: PartialEq + PartialOrd + Copy + Clone + Default + TTransformable<T> + TCoordinate<T>,
+    T: TValue {}
 
 #[allow(dead_code)]
 #[derive(Debug, Copy, Clone)]
-pub enum EPoint {
-    SPoint3D (SPoint3D),
-    SPoint2D (SPoint2D),
+pub enum EPoint<T> where T: TValue {
+    SPoint3D (SPoint3D<T>),
+    // SPoint2D (SPoint2D<T>),
 }
 
 #[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
-pub struct SPoint3D {
-    x: i32,
-    y: i32,
-    z: i32,
+pub struct SPoint3D<T> where T: TValue {
+    x: T,
+    y: T,
+    z: T,
 }
 
-impl TTransformable for SPoint3D {
-    fn shift<T: TCoordinate>(&mut self, coord: &T) {
-        self.x += coord.get_axis_value(Axis::X);
-        self.y += coord.get_axis_value(Axis::Y);
-        self.z += coord.get_axis_value(Axis::Z);
+impl<T> std::ops::MulAssign<T> for SPoint3D<T> where T: TValue {
+    fn mul_assign(&mut self, rhs: T) {
+        self.x = self.x.mul(rhs);
+        self.y = self.y.mul(rhs);
+        self.z = self.z.mul(rhs);
+    }
+}
+
+impl<T> std::ops::MulAssign for SPoint3D<T> where T: TValue {
+    fn mul_assign(&mut self, rhs: SPoint3D<T>) {
+        self.x = self.x.mul(rhs.x);
+        self.y = self.y.mul(rhs.y);
+        self.z = self.z.mul(rhs.z);
+    }
+}
+
+impl<T> TTransformable<T> for SPoint3D<T> where T: TValue {
+    fn shift<U>(&mut self, coord: &U)
+    where U: TCoordinate<T> {
+        self.x = self.x.add(*coord.get_axis_value(Axis::X));
+        self.y = self.y.add(*coord.get_axis_value(Axis::Y));
+        self.z = self.z.add(*coord.get_axis_value(Axis::Z));
     }
 
-    fn shift_axis(&mut self, axis: Axis, value: i32) {
+    fn shift_axis(&mut self, axis: Axis, value: T) {
         match axis {
-            Axis::X => self.x += value,
-            Axis::Y => self.y += value,
-            Axis::Z => self.z += value,
+            Axis::X => self.x = self.x.add(value),
+            Axis::Y => self.y = self.y.add(value),
+            Axis::Z => self.z = self.z.add(value),
         }
     }
 
@@ -65,22 +92,23 @@ impl TTransformable for SPoint3D {
     }
 
     fn reflect(&mut self) {
-        self.x *= -1;
-        self.y *= -1;
-        self.z *= -1;
+        // self.x *= -1;
+        // self.y *= -1;
+        // self.z *= -1;
     }
 
     fn reflect_axis(&mut self, axis: Axis) {
-        match axis {
-            Axis::X => self.x *= -1,
-            Axis::Y => self.y *= -1,
-            Axis::Z => self.z *= -1,
-        }
+        // match axis {
+        //     Axis::X => self.x *= -1,
+        //     Axis::Y => self.y *= -1,
+        //     Axis::Z => self.z *= -1,
+        // }
     }
 }
 
-impl TCoordinate for SPoint3D {
-    fn get_axis_value(&self, axis: Axis) -> &i32 {
+impl<T> TCoordinate<T> for SPoint3D<T>
+where T: TValue {
+    fn get_axis_value(&self, axis: Axis) -> &T {
         match axis {
             Axis::X => return &self.x,
             Axis::Y => return &self.y,
@@ -88,7 +116,7 @@ impl TCoordinate for SPoint3D {
         }
     }
 
-    fn set_axis_value(&mut self, axis: Axis, value: i32) {
+    fn set_axis_value(&mut self, axis: Axis, value: T) {
         match axis {
             Axis::X => self.x = value,
             Axis::Y => self.y = value,
@@ -97,73 +125,73 @@ impl TCoordinate for SPoint3D {
     }
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
-pub struct SPoint2D {
-    x: i32,
-    y: i32,
-}
+// #[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
+// pub struct SPoint2D {
+//     x: i32,
+//     y: i32,
+// }
 
-#[allow(dead_code)]
-impl SPoint2D {
-    pub fn flip(&mut self) {
-        self.flip_axis(Axis::X, Axis::Y);
-    }
-}
+// #[allow(dead_code)]
+// impl SPoint2D {
+//     pub fn flip(&mut self) {
+//         self.flip_axis(Axis::X, Axis::Y);
+//     }
+// }
 
-impl TTransformable for SPoint2D {
-    fn shift<T: TCoordinate>(&mut self, coord: &T) {
-        self.x += coord.get_axis_value(Axis::X);
-        self.y += coord.get_axis_value(Axis::Y);
-    }
+// impl TTransformable for SPoint2D {
+//     fn shift<T: TCoordinate>(&mut self, coord: &T) {
+//         self.x += coord.get_axis_value(Axis::X);
+//         self.y += coord.get_axis_value(Axis::Y);
+//     }
 
-    fn shift_axis(&mut self, axis: Axis, value: i32) {
-        match axis {
-            Axis::X => self.x += value,
-            Axis::Y => self.y += value,
-            _ => panic!(ERR_2D_UNSUPP_AXIS),
-        }
-    }
+//     fn shift_axis(&mut self, axis: Axis, value: i32) {
+//         match axis {
+//             Axis::X => self.x += value,
+//             Axis::Y => self.y += value,
+//             _ => panic!(ERR_2D_UNSUPP_AXIS),
+//         }
+//     }
 
-    fn flip_axis(&mut self, axis1: Axis, axis2: Axis) {
-        let orig_axis2_value = *self.get_axis_value(axis2);
-        self.set_axis_value(axis2, *self.get_axis_value(axis1));
-        self.set_axis_value(axis1, orig_axis2_value);
-    }
+//     fn flip_axis(&mut self, axis1: Axis, axis2: Axis) {
+//         let orig_axis2_value = *self.get_axis_value(axis2);
+//         self.set_axis_value(axis2, *self.get_axis_value(axis1));
+//         self.set_axis_value(axis1, orig_axis2_value);
+//     }
 
-    fn reflect(&mut self) {
-        self.x *= -1;
-        self.y *= -1;
-    }
+//     fn reflect(&mut self) {
+//         self.x *= -1;
+//         self.y *= -1;
+//     }
 
-    fn reflect_axis(&mut self, axis: Axis) {
-        match axis {
-            Axis::X => self.x *= -1,
-            Axis::Y => self.y *= -1,
-            _ => panic!(ERR_2D_UNSUPP_AXIS),
-        }
-    }
-}
+//     fn reflect_axis(&mut self, axis: Axis) {
+//         match axis {
+//             Axis::X => self.x *= -1,
+//             Axis::Y => self.y *= -1,
+//             _ => panic!(ERR_2D_UNSUPP_AXIS),
+//         }
+//     }
+// }
 
-impl TCoordinate for SPoint2D {
-    fn get_axis_value(&self, axis: Axis) -> &i32 {
-        match axis {
-            Axis::X => return &self.x,
-            Axis::Y => return &self.y,
-            Axis::Z => return &0,
-        }
-    }
+// impl TCoordinate for SPoint2D {
+//     fn get_axis_value(&self, axis: Axis) -> &i32 {
+//         match axis {
+//             Axis::X => return &self.x,
+//             Axis::Y => return &self.y,
+//             Axis::Z => return &0,
+//         }
+//     }
 
-    fn set_axis_value(&mut self, axis: Axis, value: i32) {
-        match axis {
-            Axis::X => self.x = value,
-            Axis::Y => self.y = value,
-            _ => panic!(ERR_2D_UNSUPP_AXIS),
-        }
-    }
-}
+//     fn set_axis_value(&mut self, axis: Axis, value: i32) {
+//         match axis {
+//             Axis::X => self.x = value,
+//             Axis::Y => self.y = value,
+//             _ => panic!(ERR_2D_UNSUPP_AXIS),
+//         }
+//     }
+// }
 
-#[allow(dead_code)]
-pub struct Line(EPoint, EPoint);
+// #[allow(dead_code)]
+// pub struct Line(EPoint, EPoint);
 
 // TODO: supporting point generics
 
